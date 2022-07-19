2bfdadf0d5f2c58c9ce116fba6e9cf129d578ee4
Kostas <langas.kostya@mail.ru>
2022-07-19 14:46:34 +0500

    выходной или нет

---
+++ Exc15/Program.cs
@@ -0,0 +1,18 @@
+﻿int n;
+
+Console.WriteLine ("Введите номер дня недели");
+n = Convert.ToInt32(Console.ReadLine());
+if (n >= 1 && n <= 7) 
+{ 
+    if (n == 1)  Console.WriteLine ("Нет");
+    if (n == 2)  Console.WriteLine ("Нет");
+    if (n == 3)  Console.WriteLine ("Нет");
+    if (n == 4)  Console.WriteLine ("Нет");
+    if (n == 5)  Console.WriteLine ("Нет");
+    if (n == 6)  Console.WriteLine ("Да");
+    if (n == 7)  Console.WriteLine ("Да");
+}
+else 
+{
+    Console.WriteLine ("Ты ошибся...");
+}
