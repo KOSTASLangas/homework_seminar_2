7858c9733790f5f070125b7b32bbb8ef6b5b1113
Kostas <langas.kostya@mail.ru>
2022-07-19 14:04:34 +0500

    вывод второй цифры числа

---
+++ Exc10/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine ("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 100 || n > 999)
+{
+    Console.WriteLine ("Неверное число");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+int a = n % 100 ;
+Console.WriteLine ("Вторая цифра числа:");
+Console.WriteLine (a / 10);
