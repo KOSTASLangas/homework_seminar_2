7f1f6a513d732c918937e1a0872e96fe111fbf95
Kostas <langas.kostya@mail.ru>
2022-07-19 14:41:03 +0500

    Нахождение третьей цифры в многозначных числах

---
+++ Exc13/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine ("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+if( n > 99) Console.WriteLine((n / 100) % 10);
+else
+{
+    Console.WriteLine ("нет третьей цифры");
+}
+
+
